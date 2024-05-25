e422dfa98fe3c36274c069c81ffd9f52639a3d90
Russkiygolod <velikotskiy93@gmail.com>
2024-05-25 20:36:42 +0300

    выбор бд обуславливается постановкой задачи

--- cmd/BookM.go
+++ cmd/BookM.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	//  БД  //
-
+	// выбор бд обуславливается постановкой задачи
 	//PostgreSQL
 	url := "postgres://" + "postgres" + ":" + "postgres" + "@" + "localhost" + ":" + "5432" + "/" + "ttt"
 	conn, err := pgxpool.ParseConfig(url)
